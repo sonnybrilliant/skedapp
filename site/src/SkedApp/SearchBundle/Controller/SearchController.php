016759cfb8104a8a145267ac5ada1c7e0ba00f21
Otto Saayman <otto@NewModelNo15.(none)>
2012-12-03 18:54:42 +0200

    Otto: Added a search form to the system

---
+++ site/src/SkedApp/SearchBundle/Controller/SearchController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace SkedApp\SearchBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use SkedApp\CoreBundle\Entity\company;
+use SkedApp\CoreBundle\Entity\Category;
+use SkedApp\CoreBundle\Entity\Consultant;
+use SkedApp\CoreBundle\Entity\Service;
+use SkedApp\SearchBundle\Form\SearchType;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * SkedApp\ConsultantBundle\Controller\ConsultantController
+ *
+ * @author Otto Saayman <otto.saayman@kaizania.co.za>
+ * @package SkedAppSearchBundle
+ * @subpackage Controller
+ * @version 0.0.1
+ */
+class SearchController extends Controller
+{
+
+    /**
+     * list consultants
+     *
+     * @return View
+     * @throws AccessDeniedException
+     */
+    public function indexAction($page = 1)
+    {
+
+        $this->get('logger')->info('search results');
+
+        $sort = $this->get('request')->query->get('sort');
+        $direction = $this->get('request')->query->get('direction', 'desc');
+
+        $options = array('sort' => $sort,
+            'direction' => $direction
+        );
+
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $this->container->get('consultant.manager')->listAll($options), $this->getRequest()->query->get('page', $page), 10
+        );
+
+        //Instantiate search form
+
+        $form = $this->createForm(new SearchType());
+
+        if ($this->getRequest()->getMethod() == 'POST') {
+            $form->bindRequest($this->getRequest());
+        }
+
+        return $this->render('SkedAppSearchBundle:Search:index.html.twig', array(
+                'pagination' => $pagination,
+                'sort_img' => '/img/sort_' . $direction . '.png',
+                'sort' => $direction,
+                'form' => $form->createView (),
+            ));
+    }
+
+    /**
+     * Ajax call services by category
+     *
+     * @param integer $categoryId
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws AccessDeniedException
+     */
+    public function ajaxGetServicesByCategoryAction($categoryId)
+    {
+        if ($this->getRequest()->isXmlHttpRequest()) {
+            $this->get('logger')->info('get services by category');
+            $results = array();
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $category = $em->getRepository('SkedAppCoreBundle:Category')->find($categoryId);
+
+            if ($category) {
+                $services = $this->get('service.manager')->getServicesByCategory($category);
+
+                if ($services) {
+                    foreach ($services as $service) {
+                        $results[] = array(
+                            'id' => $service->getId(),
+                            'name' => $service->getName()
+                        );
+                    }
+                }
+            }
+
+            $return = new \stdClass();
+            $return->status = 'success';
+            $return->count = sizeof($results);
+            $return->results = $results;
+
+            $response = new Response(json_encode($return));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        } else {
+            $this->get('logger')->warn('not a valid request, expected ajax call');
+            throw new AccessDeniedException();
+        }
+    }
+
+}
+
